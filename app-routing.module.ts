db9a3158969f5c4fb4f8e6e82bf28078e57de3c9
Abhinav Jindal <122330686+Abhi7ji@users.noreply.github.com>
2024-04-20 14:20:10 -0400

    Create app-routing.module.ts

---
+++ app-routing.module.ts
@@ -0,0 +1,17 @@
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  },
+  
+  {
+  path: 'home',
+  component: HomeComponent,
+  
+  },
+  {
+
+  path: 'register',
+  component: RegisterComponent,
+  }
+];
